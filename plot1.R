17b8ad5c1bfee3fa40af578a883fc99e3ecf5426
Ziming Zhao <zzy4987@gmail.com>
2014-12-03 15:29:02 +0800

    go

---
+++ plot1.R
@@ -0,0 +1,6 @@
+png('plot1.png',width=480,height=480)
+data<-read.table('household_power_consumption.txt',sep=';',header=T,na.strings='?',stringsAsFactors=F)
+data_sub<-subset(data,data$Date=='1/2/2007'|data$Date=='2/2/2007')
+sum(is.na(data_sub$Global_active_power))
+hist(data_sub$Global_active_power,main='Global Active Power',xlab='Global Active Power(kilowatts)',col='red')
+dev.off()
